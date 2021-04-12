b955b615c1d66c73c809bf65fcccc79418af79ac
Stone98 <stone18003@gamil.com>
2021-04-12 10:09:07 -0700

    typo

--- api/server.js
+++ api/server.js
@@ -2,10 +2,10 @@ const express = require("express");
 
 const server = express();
 
-const carsRouter = require("./cars/cars-router");
+const CarsRouter = require("./cars/cars-router");
 
 server.use(express.json());
 
-server.use("/api/cars", carsRouter);
+server.use("/api/cars", CarsRouter);
 
 module.exports = server;
