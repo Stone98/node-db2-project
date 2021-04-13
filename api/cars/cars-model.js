84cc2f48007dba75663f3c6c56e672f4fdbf4e4c
Stone98 <stone18003@gamil.com>
2021-04-13 16:49:10 -0700

    model comments

--- api/cars/cars-model.js
+++ api/cars/cars-model.js
@@ -1,16 +1,16 @@
 const db = require("../../data/db-config");
 
 const getAll = () => {
-  return db("cars");
+  return db("cars"); // returns all cars in the database
 };
 
 const getById = (id) => {
-  return db("cars").where("id", id).first();
+  return db("cars").where("id", id).first(); // returns the car who's id matched the id given first
 };
 
 const create = async (car) => {
-  const [id] = await db("cars").insert(car);
-  return getById(id);
+  const [id] = await db("cars").insert(car); // id variable that can access the newly created car
+  return getById(id); // returns the newly created car
 };
 
 module.exports = {
